e2f884bbc5813742b83ec9d7496b604f115fdbd9
Erwin Heemsbergen <erwin@heemsbergen.net>
2017-03-11 12:15:08 +0100

    Fixed e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,15 +1,13 @@
 import { browser, element, by } from 'protractor';
 
 describe('QuickStart E2E Tests', function () {
-
-  let expectedMsg = 'Hello Jane Doe';
-
   beforeEach(function () {
     browser.get('');
   });
 
-  it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  it('should display the menu', function () {
+    expect(element(by.css('ul')).getText()).toContain('Home');
+    expect(element(by.css('ul')).getText()).toContain('About');
   });
 
 });
