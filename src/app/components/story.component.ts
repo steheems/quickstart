91207b9248536306ecc60d3dd8c32b8b1bef4bf4
Erwin Heemsbergen <erwin@heemsbergen.net>
2017-03-12 03:24:12 +0100

    Ability to add extra users to story component

--- src/app/components/story.component.ts
+++ src/app/components/story.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/users.service';
 import { UserDetails } from '../interfaces/userDetails.interface';
-import { PronounType } from '../enums/pronounTypes.interface';
 
 /**
  * Class for generating a story component.
@@ -16,17 +15,31 @@ import { PronounType } from '../enums/pronounTypes.interface';
   providers: [UserService]
 })
 export class StoryComponent implements OnInit {
-  user: UserDetails;
-  pronounType: PronounType;
-  antlr: string;
+  selectedUsers: UserDetails[] = [];
+  allUsers: UserDetails[] = [];
+  newUser: UserDetails;
 
   constructor(private userService: UserService) {
-    this.pronounType = new PronounType();
+
   }
 
   ngOnInit() {
-    this.userService.getUser('58c3b119aaf712a3e02da0ee').subscribe(user => {
-      this.user = user;
+    this.userService.getUsers().subscribe(users => {
+      this.allUsers = users;
     });
+    this.userService.getUser('58c3b119aaf712a3e02da0ee').subscribe(user => {
+      this.selectedUsers.push(user);
+    });
+  }
+
+  addUser() {
+    console.log(this.selectedUsers);
+    this.selectedUsers.push(this.newUser);
+    this.newUser = null;
+    console.log(this.selectedUsers);
+  }
+
+  logSomething(i: any) {
+    console.log(i);
   }
 }
